68c91c046a04d38d86ff6a1ec66148ea0219457c
JacobH010 <jacoblharman@gmail.com>
2024-11-10 17:02:45 -0600

    Main Menu Prototype completed and scene manager established

---
+++ GameJamPrototype/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartAIStageScene()
+    {
+        SceneManager.LoadScene("AIStageing");
+    }
+    public void StartGamePrototype()
+    {
+        SceneManager.LoadScene("Prototype_OfficeLevel");
+    }
+    public void StartMovementStage()
+    {
+        Debug.Log("Movement stage not initialized in Main Menu Manager");
+    }
+}
