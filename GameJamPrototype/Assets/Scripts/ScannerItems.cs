2f475c7521f594c227d9f7e0620a2d0e183bd1fc
JacobH010 <jacoblharman@gmail.com>
2024-11-12 19:05:26 -0600

    Trying and failing to get it working

--- GameJamPrototype/Assets/Scripts/ScannerItems.cs
+++ GameJamPrototype/Assets/Scripts/ScannerItems.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScannerItems : DragItemScript
+public class ScannerItems : DragCashScript
 {
     
     // Start is called before the first frame update
