43bc5068eb577a91df54796d7f6887532afe7d8e
Chesticle <emmieharman1731vrdw@gmail.com>
2024-11-30 00:13:43 -0600

    Added a tutorial balanced loadout manager

--- GameJamPrototype/Assets/Scripts/VideoController.cs
+++ GameJamPrototype/Assets/Scripts/VideoController.cs
@@ -3,12 +3,41 @@ using UnityEngine.Video;
 
 public class VideoController : MonoBehaviour
 {
-    public VideoPlayer videoPlayer; // Assign the VideoPlayer
-    public GameObject playButton;  // Assign the Play Button (to hide it when playing)
+    public GameObject videoPlayerObject; // The GameObject with the VideoPlayer component
+    public GameObject playButton;       // Play Button GameObject
+    public GameObject closeButton;      // Close Button GameObject
 
     public void PlayVideo()
     {
-        videoPlayer.Play();        // Play the video
-        playButton.SetActive(false); // Hide the button while the video is playing
+        // Enable the VideoPlayerObject
+        videoPlayerObject.SetActive(true);
+
+        // Get the VideoPlayer component and start playback
+        VideoPlayer videoPlayer = videoPlayerObject.GetComponent<VideoPlayer>();
+        if (videoPlayer != null)
+        {
+            videoPlayer.Play();
+        }
+
+        // Show the Close Button and hide the Play Button
+        playButton.SetActive(false);
+        closeButton.SetActive(true);
+    }
+
+    public void StopVideo()
+    {
+        // Get the VideoPlayer component and stop playback
+        VideoPlayer videoPlayer = videoPlayerObject.GetComponent<VideoPlayer>();
+        if (videoPlayer != null)
+        {
+            videoPlayer.Stop();
+        }
+
+        // Disable the VideoPlayerObject
+        videoPlayerObject.SetActive(false);
+
+        // Show the Play Button and hide the Close Button
+        playButton.SetActive(true);
+        closeButton.SetActive(false);
     }
 }
